2bc5caddc5a9635ead673095d5ca359206d402ae
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2024-03-25 18:46:52 -0600

    Authentication added

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,3 +1,8 @@
+
+import { Button } from '@/components/ui/button'
+import { ClerkLoaded, ClerkLoading, SignedIn, UserButton, SignInButton, SignedOut } from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
@@ -5,7 +10,44 @@ const Header = () => {
     <header
         className='h-20 w-full border-b-2 border-slate-200 px-4'
     >
-        Header
+       <div
+        className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full '
+       >
+            <div
+                className='pt-8 pl-4 pb-7 flex items-center gap-x-3'
+            >
+                <Image 
+                    src={"/mascot.svg"}
+                    width={40}
+                    height={40}
+                    alt='logo'
+                />
+                <h1
+                    className='text-2xl font-extrabold text-green-600 tracking-wide'
+                >Lingo</h1>
+            </div>
+            <ClerkLoading>
+                <Loader className='h-5 w-5 text-muted-foreground animate-spin'/>
+            </ClerkLoading>
+            <ClerkLoaded>
+                <SignedIn>
+                    <UserButton
+                        afterSignOutUrl='/'
+                    />
+                </SignedIn>
+                <SignedOut>
+                    <SignInButton
+                        mode='modal'
+                        afterSignInUrl='/learn'
+                        afterSignUpUrl='/learn'
+                    >
+                        <Button size={"lg"} variant={"ghost"}>
+                            Login
+                        </Button>
+                    </SignInButton>
+                </SignedOut>
+            </ClerkLoaded>
+       </div>
     </header>
   )
 }
